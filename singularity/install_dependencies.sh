9935f257331d72e3581072e40eda52cb6f9f4262
Kapper <knaldhat@gmail.com>
2020-04-01 14:08:34 +0200

    test without CUDA

--- singularity/install_dependencies.sh
+++ singularity/install_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+cd ~
 #installs dependencies required for compiling singularity from source
 #from: https://sylabs.io/guides/3.5/admin-guide/installation.html
 sudo apt-get update
@@ -22,12 +23,12 @@ sudo apt-get update
 sudo apt-get install -y nvidia-driver-440
 
 # Install CUDA 10.2 host driver for base image to use.
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-sudo apt-get update
-sudo apt-get -y install cuda
+#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+#sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+#sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+#sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+#sudo apt-get update
+#sudo apt-get -y install cuda
 
 # Install R
 export R_VERSION=3.6.3
