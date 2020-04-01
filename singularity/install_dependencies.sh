384065183fb4fe1e9ea836e7e62b4f5a5169c0de
Kapper <knaldhat@gmail.com>
2020-04-01 14:08:09 +0200

    add install R+RStudio

--- singularity/install_dependencies.sh
+++ singularity/install_dependencies.sh
@@ -28,3 +28,26 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
 sudo apt-get -y install cuda
+
+# Install R
+export R_VERSION=3.6.3
+echo "deb http://cran.r-project.org/bin/linux/ubuntu bionic-cran35/" > /etc/apt/sources.list.d/r.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+apt-get update
+apt-get install -y --no-install-recommends \
+  r-base=${R_VERSION}* \
+  r-base-core=${R_VERSION}* \
+  r-base-dev=${R_VERSION}* \
+  r-recommended=${R_VERSION}* \
+  r-base-html=${R_VERSION}* \
+  r-doc-html=${R_VERSION}* \
+  libcurl4-openssl-dev \
+  libssl-dev \
+  libxml2-dev \
+  libcairo2-dev \
+  libxt-dev
+
+#install RStudio
+wget -q https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.5033-amd64.deb
+sudo dpkg -i rstudio-1.2.5033-amd64.deb
+rm rstudio-1.2.5033-amd64.deb
