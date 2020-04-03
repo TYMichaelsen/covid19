93e9c5c7687b94637ab3418c280f082ff9ad0355
Kapper <knaldhat@gmail.com>
2020-04-03 14:37:41 +0200

    let's lock versions

--- singularity/prep_build.sh
+++ singularity/prep_build.sh
@@ -7,10 +7,16 @@ if [ ! -f ./build/ont-guppy_3.4.3_linux64.tar.gz ] ; then
 fi
 if [ ! -d ./build/artic-ncov2019 ]; then
 	git clone --recursive https://github.com/artic-network/artic-ncov2019.git build/artic-ncov2019
+	cd build/artic-ncov2019
+	git checkout 2127aef
+	cd -
 fi
-if [ ! -f build/miniconda.sh ] ; then
+if [ ! -f ./build/miniconda.sh ] ; then
 	wget -q https://repo.anaconda.com/miniconda/Miniconda3-4.7.12.1-Linux-x86_64.sh -O build/miniconda.sh
 fi
 if [ ! -d ./build/auspice ]; then
 	git clone https://github.com/nextstrain/auspice.git build/auspice
+	cd build/auspice
+	git checkout -q v2.11.3
+	cd -
 fi
