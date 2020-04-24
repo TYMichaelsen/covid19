1e1f00389d7626109f4774423238c48d935f0739
root <root@hurtigholger>
2020-04-24 12:17:07 +0200

    added updatescript for workstations (run with alias='su -c updatecovid19.sh'

---
+++ singularity/updatecovid19.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dir=/opt/covid19
+gitdir=${dir}/covid19
+mkdir -p $gitdir
+rm -rf $gitdir
+
+git clone -b master https://github.com/kasperskytte/covid19.git $gitdir
+if [ ! -f ${dir}/covid19_1.5.sif ]
+then
+  singularity pull -F covid19_1.5.sif library://kasperskytte/default/covid19:1.5
+fi
+cd ${dir}
+singularity build -F covid19_latest.sif ${gitdir}/singularity/covid19.singularity
