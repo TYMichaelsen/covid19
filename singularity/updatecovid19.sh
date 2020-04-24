51a2e541b7a752c254aa401dbbfd1e04b1df523a
root <root@hurtigholger>
2020-04-24 14:27:29 +0200

    update script from workstations branch

--- singularity/updatecovid19.sh
+++ singularity/updatecovid19.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 dir=/opt/covid19
 gitdir=${dir}/covid19
 mkdir -p $gitdir
 rm -rf $gitdir
 
-git clone -b master https://github.com/kasperskytte/covid19.git $gitdir
+git clone -b workstations https://github.com/kasperskytte/covid19.git $gitdir
 if [ ! -f ${dir}/covid19_1.5.sif ]
 then
   singularity pull -F covid19_1.5.sif library://kasperskytte/default/covid19:1.5
