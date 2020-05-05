4ebb744fa7f78b2be1af7a800ddf0320ea4202bf
Vang Le-Quy <vle@covid1>
2020-05-05 09:07:20 +0200

    Fix bindings for auspiceView.sh

--- workflow/auspiceView.sh
+++ workflow/auspiceView.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 SINGIMG="/srv/rbd/covid19/thecontainer/covid19_latest.sif"
 MYPORT=$(shuf -i 4000-6500 -n 1)
-DATDIR=${1:-/srv/rbd/covid19/current/analysis/nextstrain/auspice}
-singularity exec $SINGIMG bash -c "source activate nextstrain; PORT=$MYPORT auspice view --datasetDir $DATDIR"
+DATDIR=${1:-/srv/rbd/covid19/current/analysis/nextstrain/auspice/}
+singularity exec -B $DATDIR:$DATDIR -B /srv/rbd:/srv/rbd $SINGIMG bash -c "source activate nextstrain; PORT=$MYPORT auspice view --datasetDir $DATDIR"
