7b2af3754e7ba3c1bc1cf7c63f8ce30bd775836e
Vang Le-Quy <vle@covid1>
2020-05-05 11:11:02 +0200

    Correct path resolving in auspiceView.sh

--- workflow/auspiceView.sh
+++ workflow/auspiceView.sh
@@ -2,4 +2,5 @@
 SINGIMG="/srv/rbd/covid19/thecontainer/covid19_latest.sif"
 MYPORT=$(shuf -i 4000-6500 -n 1)
 DATDIR=${1:-/srv/rbd/covid19/current/analysis/nextstrain/auspice/}
+DATDIR=$(readlink -f $DATDIR)
 singularity exec -B $DATDIR:$DATDIR -B /srv/rbd:/srv/rbd $SINGIMG bash -c "source activate nextstrain; PORT=$MYPORT auspice view --datasetDir $DATDIR"
