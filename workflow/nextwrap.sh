f1a5109e030c9f8467bb94c93a3c67ef356b01d1
Vang Le-Quy <vle@covid1>
2020-05-05 10:48:14 +0200

    Tune nextwrap paths

--- workflow/nextwrap.sh
+++ workflow/nextwrap.sh
@@ -7,12 +7,25 @@ echo "$0 /srv/rbd/covid19/current/export/2020_04_28_12-27_export/sequences.fasta
 SINGIMG="/srv/rbd/covid19/thecontainer/covid19_latest.sif"
 # DISTDIR="/srv/rbd/covid19/current"
 DISTDIR="/srv/rbd"
+
 GENOMEFASTA="$1"
 METAFILE="$2"
+GENOMEDIR=$(dirname $GENOMEFASTA)
+METADIR=$(dirname $METAFILE)
 OUTDIR="${3:-$PWD/nextstrainRun}"
 THISDIR=$(dirname $(readlink -f $0))
 NEXTSTRAIN_SCRIPT=${THISDIR}/nextstrain.sh
 
-echo "Running comand:"
-echo "singularity exec  -B $DISTDIR:$DISTDIR -B $HOME:$HOME $SINGIMG bash -c \"source activate nextstrain; $NEXTSTRAIN_SCRIPT -s $GENOMEFASTA -m $METAFILE -o $OUTDIR\""
-singularity exec  -B $DISTDIR:$DISTDIR -B $HOME:$HOME $SINGIMG bash -c "source activate nextstrain; $NEXTSTRAIN_SCRIPT -s $GENOMEFASTA -m $METAFILE -o $OUTDIR"
+echo -e "Running comand:\n---"
+echo "singularity exec  -B $DISTDIR:$DISTDIR
+-B $HOME:$HOME
+-B $METADIR:$METADIR
+-B GENOMEDIR:$GENOMEDIR
+$SINGIMG bash -c \"source activate nextstrain; $NEXTSTRAIN_SCRIPT -s $GENOMEFASTA -m $METAFILE -o $OUTDIR\""
+echo "---"
+
+singularity exec  -B $DISTDIR:$DISTDIR \
+            -B $HOME:$HOME \
+            -B $METADIR:$METADIR \
+            -B $GENOMEDIR:$GENOMEDIR \
+            $SINGIMG bash -c "source activate nextstrain; $NEXTSTRAIN_SCRIPT -s $GENOMEFASTA -m $METAFILE -o $OUTDIR"
