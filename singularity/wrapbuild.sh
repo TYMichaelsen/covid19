e6517a45abe79a39037f957a640fa0dc0c93bb87
Kapper <knaldhat@gmail.com>
2020-05-07 14:36:46 +0200

    timestamp with HH:MM too for if built more than once a day

--- singularity/wrapbuild.sh
+++ singularity/wrapbuild.sh
@@ -42,7 +42,7 @@ installImage(){
     if [[ ! -f ${THISDIR}/${FINALIMG} ]]; then
         buildImage
     else
-    	local TimeStamp=$(date +%Y%m%d)
+    	local TimeStamp=$(date +%y%m%d_%H%M)
         cp ${THISDIR}/${FINALIMG} ${INSTDIR}/covid19_${TimeStamp}.sif
         cd ${INSTDIR}
         ln -snf covid19_${TimeStamp}.sif covid19_latest.sif
