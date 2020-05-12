9776bf87eaece7e325ad16b2fb5e7785690ff731
TYMichaelsen <tym@bio.aau.dk>
2020-05-12 11:49:07 +0200

    moved parallel-bit outside to parallel.snippet.sh

--- workflow/upload.sh
+++ workflow/upload.sh
@@ -105,28 +105,12 @@ for i in "${ARTICDIR[@]}"; do
   for j in $i/articminion/*.consensus.fasta; do echo $j; done
 done | sed 's/.consensus.fasta/.sorted.bam/' - > upload/$DT/tmp_bamfiles
 
-# THE FOLLOWING NEEDS SINGULARITY TO RUN:--------------------------------------
-#------------------------------------------------------------------------------
-source activate artic-ncov2019-medaka
+# Make input file to parallel.
+awk -F'\t' -v outdir=upload/$DT -v human=$HUMANREF '{print $1":"$2":"outdir":"human}' upload/$DT/tmp_toexport.txt > upload/$DT/tmp_toparallel.txt
 
-# for each mapping, run sanitizeme.
-awk -F'\t' -v outdir=upload/$DT -v human=$HUMANREF '{print $1":"$2":"outdir":"human}' upload/$DT/tmp_toexport.txt | parallel -j $THREADS --colsep ':' --bar \
-'
-# Get the corresponding .bam file.
-BAMFILE=$(grep {2}_ {3}/tmp_bamfiles)
-
-if [ -z "$BAMFILE" ]; then
-  >&2 echo "warning: .bam file for {1} (LIB-ID: {2}) was not in the artic output." 
-else
-  # Extract mapped reads and remove human reads with the CDC protocol: https://github.com/CDCgov/SanitizeMe
-  samtools fastq --threads 1 -F 4 $BAMFILE 2> /dev/null |\
-  minimap2 -ax map-ont {4} - -t 1 2> /dev/null |\
-  samtools view --threads 1 -u -f 4 - 2> /dev/null |\
-  samtools bam2fq --threads 1 - 2> /dev/null |\
-  gzip -c - > {3}/mapped_fastq/"$(sed s/\\//_/g <<< {1})".fastq.gz 2> /dev/null
-fi' 
-
-#------------------------------------------------------------------------------
+# Run in singularity.
+SINGIMG="/srv/rbd/thecontainer/covid19_latest.sif"
+singularity --silent exec -B /srv/rbd:/srv/rbd $SINGIMG bash -c "DT=$DT; THREADS=$THREADS; . $WORKFLOW_PATH/parallel.snipet.sh"
 
 echo "md5 checksum"
 
