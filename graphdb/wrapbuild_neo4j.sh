959cd78cb292eb3560ced50973ac344a0c0b328d
Tomer Sagi <tsagi@cs.aau.dk>
2020-06-04 15:03:44 +0200

    init wrapbuild for neo4j container

---
+++ graphdb/wrapbuild_neo4j.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+if [ "$1" != "install" ] && [ "$1" != "build" ]
+then
+	echo "Usage: $0 [install|build]"
+	exit 1
+fi
+
+BUILDDIR=/tmp/$UID
+mkdir -p $BUILDDIR
+
+BASEIMG="covid19_1.5.sif"
+FINALIMG="covid19_latest.sif"
+
+THISDIR=$PWD
+REPONAME=${THISDIR##*/}
+REPODIR=$(dirname ${THISDIR})
+
+buildImage(){
+    # Handle build under /srv/rbd
+    if [[ "$THISDIR" == "/srv/rbd"* ]]; then
+        echo Cleaning up $BUILDDIR ...
+        rm -rf ${BUILDDIR}
+        echo Copying files to $BUILDDIR ...
+        cp -a ${REPODIR} ${BUILDDIR}/ 
+        cd ${BUILDDIR}/${REPONAME}
+        echo Working under $PWD ...
+        make
+        if [ $? -eq 0 ]; then
+            mv $BASEIMG $FINALIMG  ${THISDIR}/
+            rm -rf ${BUILDDIR}
+        else
+            echo "Build failed. Check your defition file or build command"
+        fi
+
+    else # build under $HOME
+        make
+    fi
+}
+
+installImage(){
+    INSTDIR="/srv/rbd/thecontainer" 
+    if [[ ! -f ${THISDIR}/${FINALIMG} ]]; then
+        buildImage
+    fi
+    local TimeStamp=$(date +%y%m%d_%H%M)
+    cp ${THISDIR}/${FINALIMG} ${INSTDIR}/covid19_${TimeStamp}.sif
+    cd ${INSTDIR}
+    ln -snf covid19_${TimeStamp}.sif covid19_latest.sif
+    echo Image installed and linked:
+    ls -lah ${INSTDIR}/covid19_latest.sif
+}
+
+if [ "$1" == 'build' ]; then buildImage; fi
+if [ "$1" == 'install' ]; then installImage; fi
