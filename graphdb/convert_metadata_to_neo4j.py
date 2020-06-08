093c6dc3dea12c2a8e900e626066dccf08d6b290
Tomer Sagi <ts.tomersagi@gmail.com>
2020-06-08 12:32:59 +0200

    fixed hostname in neo4j

--- graphdb/convert_metadata_to_neo4j.py
+++ graphdb/convert_metadata_to_neo4j.py
@@ -4,7 +4,7 @@ import csv
 host = input("Please enter the neo4j database server IP (defaults to localhost): ")
 host = 'localhost' if len(host.strip(' ')) == 0 else host
 password = input("Please enter the neo4j user's database password: ")
-graph = Graph("bolt://localhost:7687",user='neo4j', password=password)  # this may need to change when running from the server since there it needs the instance IP
+graph = Graph("bolt://{}:7687".format(host),user='neo4j', password=password)  # this may need to change when running from the server since there it needs the instance IP
 graph.delete_all()
 
 # Create schema
@@ -51,4 +51,7 @@ with open('../bi_system/stable_dims/age_groups.txt') as csvfile:
 p = Node("Person", ssi_id="example_id")
 tx.create(Relationship(p,"ISA",sex_m))
 tx.create(Relationship(p,"ISA",age_groups['40-49']))
+p1 = Node("Person", ssi_id="example_id2")
+tx.create(Relationship(p1,"ISA",sex_m))
+tx.create(Relationship(p2,"ISA",age_groups['40-49']))
 tx.commit()
