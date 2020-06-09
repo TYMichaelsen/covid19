1ec47a0cec3c0baf8275343b462f80124e373756
Tomer Sagi <ts.tomersagi@gmail.com>
2020-06-09 11:02:35 +0200

    converted age to an integer

--- graphdb/convert_metadata_to_neo4j.py
+++ graphdb/convert_metadata_to_neo4j.py
@@ -102,7 +102,8 @@ with open('/srv/rbd/covid19/metadata/2020-05-26-07-35_metadata.tsv') as csvfile:
     reader = csv.DictReader(csvfile, delimiter='\t')
     for row in reader:
         cv_stat = row['COVID19_Status'] if len(row['COVID19_Status']) > 0 else '0'  # error correction
-        p = Node("Person", ssi_id=row['ssi_id'], age=row['ReportAge'], COVID19_Status=cv_stat,
+        age = row['ReportAge'] if row['ReportAge']=='' else int(row['ReportAge'])
+        p = Node("Person", ssi_id=row['ssi_id'], age=age, COVID19_Status=cv_stat,
                  COVID19_EndDate=row['COVID19_EndDate'], isPregnant=(row['Pregnancy'] == '1'), sequenced=(row['sequenced'] == 'Yes'))
         if (row['Pregnancy'] == '1' and row['Sex'] == 'M'):
             print('anomalous case data') # TODO extract all error checking code to a separate file
