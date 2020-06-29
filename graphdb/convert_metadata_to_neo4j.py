2f1fe925f6fe02691a7f9151896339181d11edde
Tomer Sagi <ts.tomersagi@gmail.com>
2020-06-29 12:20:56 +0200

    refactored neo4j loader to work with params

--- graphdb/convert_metadata_to_neo4j.py
+++ graphdb/convert_metadata_to_neo4j.py
@@ -1,106 +1,24 @@
 from py2neo import Graph, Node, Relationship
+import argparse
 import csv
-
-host = input("Please enter the neo4j database server IP (defaults to localhost): ")
-host = 'localhost' if len(host.strip(' ')) == 0 else host
-password = input("Please enter the neo4j user's database password: ")
-graph = Graph("bolt://{}:7687".format(host), user='neo4j',
-              password=password)  # this may need to change when running from the server since there it needs the instance IP
-graph.delete_all()
-
-# Create schema
-tx = graph.begin()
-
-## Relationships
-ISA = Relationship.type("ISA")
-
-## Sex's
-sex_m = Node("Sex", name="M")
-sex_f = Node("Sex", name="F")
-tx.create(sex_m)
-tx.create(sex_f)
-
-## Age groups
-age_groups = {}
-with open('../bi_system/stable_dims/age_groups.tsv') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        n = Node("AgeGroup", name=row[0])
-        age_groups[row[0]] = n
-        tx.create(n)
-
-## Geodata
-parishes = {}
-with open('../bi_system/stable_dims/parish_ses.tsv') as csvfile:
-    reader = csv.reader(csvfile, delimiter='\t')
-    next(reader, None)
-    for row in reader:
-        n = Node("Parish", code=row[0], name=row[1], population=int(row[2]), ghetto_area=row[3])
-        parishes[row[0]] = n
-        tx.create(n)
-
-municipalities = {}
-with open('../bi_system/stable_dims/municipalities.tsv') as csvfile:
-    reader = csv.reader(csvfile, delimiter='\t')
-    next(reader, None)
-    for row in reader:
-        n = Node("Municipality", code=row[0], name=row[1], population=int(row[4]))
-        municipalities[row[0]] = n
-        tx.create(n)
-
-countries = {}
-dk = Node("Country", name='Denmark', nuts_code='DK0')
-countries['Denmark'] = tx.create(dk)
-nuts3_regions = {}
-with open('../bi_system/stable_dims/nuts3_regions.tsv') as csvfile:
-    reader = csv.reader(csvfile, delimiter='\t')
-    next(reader, None)
-    for row in reader:
-        n = Node("NUTS3_Region", code=row[0], name=row[1])
-        nuts3_regions[row[0]] = n
-        tx.create(n)
-        tx.create(Relationship(n,"PartOf",dk))
+import os
 
 
-countries = {}
-
-## Virus Strains
-strains = {}
-
-## Medical History
-risk_factors = {}
-with open('../bi_system/stable_dims/risk_factors.tsv') as csvfile:
-    reader = csv.reader(csvfile, delimiter='\t')
-    next(reader, None)
-    for row in reader:
-        n = Node("RiskFactor", code=row[0], name=row[1])
-        risk_factors[row[0]] = n
-        tx.create(n)
-
-print("Created graph schema")
+def log_field_error(field_name: str, row_num: int, err_msg: str, logfilewriter: csv.DictWriter) -> None:
+    """
+    Utility to log parameter errors
+    :param field_name: field where error was found
+    :param row_num: row number where error was found
+    :param err_msg
+    :param logfilewriter: for output
+    :return: None
+    """
+    logfilewriter.writerow(
+        {'MessageType': 'Error', 'Row': row_num, 'ErrorType': 'Value Error',
+         'Details': 'Error in {}, details: {}'.format(field_name, err_msg)})
 
 
-# CREATE (A0_9:AgeGroup {name: '0-9'})
-# CREATE (A10_19:AgeGroup {name: '10-19'})
-# CREATE (A20_29:AgeGroup {name: '20-29'})
-# CREATE (A30_39:AgeGroup {name: '30-39'})
-# CREATE (A40_49:AgeGroup {name: '40-49'})
-# CREATE (A40_49:AgeGroup {name: '50-59'})
-# CREATE (A40_49:AgeGroup {name: '60-69'})
-# CREATE (A40_49:AgeGroup {name: '70-79'})
-# CREATE (A40_49:AgeGroup {name: '80-89'})
-# CREATE (A40_49:AgeGroup {name: '90+'})
-#
-
-# CREATE (SM:Sex {name: 'M'})
-# CREATE (SF:Sex {name: 'F'})
-#
-# ## Countries
-# CREATE (IT:Country {name:'Italy'})
-# CREATE (P1:Patient {ssi_id:'SSI-123', sex:'F', age: '42', pregnancy: 0})
-
-
-def make_rel(with_node, code_field_name, lookup_dict, relation_name, rel_node_label, name_field_name=None):
+def make_rel(tx, row, with_node, code_field_name, lookup_dict, relation_name, rel_node_label, name_field_name=None):
     rel_node = None
     rel_node_key = row[code_field_name]
     if len(rel_node_key) > 0:
@@ -118,72 +36,194 @@ def make_rel(with_node, code_field_name, lookup_dict, relation_name, rel_node_la
     return rel_node
 
 
-# Create data
-with open('/srv/rbd/covid19/metadata/2020-05-26-07-35_metadata.tsv') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter='\t')
-    for row in reader:
-        age = row['ReportAge'] if row['ReportAge']=='' else int(row['ReportAge'])
-        cv_stat = row['COVID19_Status'] if len(row['COVID19_Status']) > 0 else '0'  # error correction
-        p = Node("Person", ssi_id=row['ssi_id'], age=age, COVID19_Status=cv_stat,
-                 COVID19_EndDate=row['COVID19_EndDate'], IsPregnant=(row['Pregnancy'] == '1')
-                 , sequenced=(row['sequenced'] == 'Yes'), SampleDate = row['SampleDate'],
-                 SymptomsStartDate=row['SymptomsStartDate'])
+def check_file(filepath, create=False):
+    """
+    Checks if the supplied path contains a file, if second argument is true, creates the file if missing
+    :type filepath: str
+    :param filepath: relative path to check
+    :type create: bool
+    :param create: defaults to false , if true will create the file
+    :return: absolute filepath if it does, print error message and exit otherwise
+    """
+    if len(filepath) == 0:
+        print("invalid filepath")
+        exit(-1)
 
-        if (row['Pregnancy'] == '1' and row['Sex'] == 'M'):
-            print('anomalous case data') # TODO extract all error checking code to a separate file
-            print('SSI {}, Pregnancy {}, Sex {}'.format(row['ssi_id'],row['Pregnancy'],row['Sex']))
+    if not os.path.exists(filepath):
+        if create:
+            with open(filepath, 'w'):
+                pass
+        else:
+            print("Invalid filepath: {}".format(filepath))
+            exit(-1)
 
-        tx.create(p)
-        if row['Sex'] == 'F':
-            tx.create(Relationship(p, "ISA", sex_f))
-        elif row['Sex'] == 'M':
-            tx.create(Relationship(p, "ISA", sex_m))
-        # else: # make error log file
-        #  print('Unrecognized Sex value: {}'.format(row['Sex']))
-        ag = row['ReportAgeGrp']
-        if ag in age_groups.keys():
-            tx.create(Relationship(p, "InGroup", age_groups[ag]))
-        # TODO report missing ag
-
-        # Parish
-        parish = make_rel(with_node=p, code_field_name='Parishcode', lookup_dict=parishes, relation_name="LivesIn",
-                          rel_node_label="Parish", name_field_name="ParishName")
-
-        # Municipality
-        if parish is not None:
-            muni = make_rel(with_node=parish, code_field_name='MunicipalityCode', lookup_dict=municipalities, relation_name="PartOf",
-                 rel_node_label="Municipality")
-
-        # NUTS3 Region
-        if muni is not None:
-            region = make_rel(with_node=muni, code_field_name='NUTS3Code', lookup_dict=nuts3_regions, relation_name="PartOf",
-                 rel_node_label="NUTS3_Region", name_field_name="NUTS3Text")
+    return filepath
 
 
-        # strains
-        strain_name = row['lineage']
-        if len(strain_name) > 0:
-            make_rel(with_node=p,code_field_name='lineage',lookup_dict=strains, relation_name="HasStrain",
-                     rel_node_label="Strain")
+def create_dims(graph):
+    # Create schema
+    tx = graph.begin()
 
-        # Risk factors
-        for field_name in risk_factors.keys():
-            if row[field_name] in ['SAND','TRUE']:
-                tx.create(Relationship(p, "HasRisk", risk_factors[field_name]))
+    # Relationships
+    ISA = Relationship.type("ISA")
+
+    # Sex's
+    sex_m = Node("Sex", name="M")
+    sex_f = Node("Sex", name="F")
+    tx.create(sex_m)
+    tx.create(sex_f)
+
+    # Age groups
+    age_groups = {}
+    with open('../bi_system/stable_dims/age_groups.tsv') as dimfile:
+        reader = csv.reader(dimfile)
+        for row in reader:
+            n = Node("AgeGroup", name=row[0])
+            age_groups[row[0]] = n
+            tx.create(n)
+
+    # Geodata
+    parishes = {}
+    with open('../bi_system/stable_dims/parish_ses.tsv') as pfile:
+        reader = csv.reader(pfile, delimiter='\t')
+        next(reader, None)
+        for row in reader:
+            n = Node("Parish", code=row[0], name=row[1], population=int(row[2]), ghetto_area=row[3])
+            parishes[row[0]] = n
+            tx.create(n)
+
+    municipalities = {}
+    with open('../bi_system/stable_dims/municipalities.tsv') as mfile:
+        reader = csv.reader(mfile, delimiter='\t')
+        next(reader, None)
+        for row in reader:
+            n = Node("Municipality", code=row[0], name=row[1], population=int(row[4]))
+            municipalities[row[0]] = n
+            tx.create(n)
+
+    countries = {}
+    dk = Node("Country", name='Denmark', nuts_code='DK0')
+    countries['Denmark'] = tx.create(dk)
+    nuts3_regions = {}
+    with open('../bi_system/stable_dims/nuts3_regions.tsv') as rfile:
+        reader = csv.reader(rfile, delimiter='\t')
+        next(reader, None)
+        for row in reader:
+            n = Node("NUTS3_Region", code=row[0], name=row[1])
+            nuts3_regions[row[0]] = n
+            tx.create(n)
+            tx.create(Relationship(n, "PartOf", dk))
+
+    # Virus Strains
+    strains = {}
+
+    # Medical History
+    risk_factors = {}
+    with open('../bi_system/stable_dims/risk_factors.tsv') as rskfile:
+        reader = csv.reader(rskfile, delimiter='\t')
+        next(reader, None)
+        for row in reader:
+            n = Node("RiskFactor", code=row[0], name=row[1])
+            risk_factors[row[0]] = n
+            tx.create(n)
+
+    tx.commit()
+    print("Created dimensions")
+    return {'parishes': parishes, 'sex_m': sex_m, 'sex_f': sex_f, 'municipalities': municipalities,
+            'nuts3_regions': nuts3_regions, 'risk_factors': risk_factors, 'strains': strains, 'countries': countries}
 
 
-        # Place of infection
-        country = row['PlaceOfInfection_EN']
-        if len(country) > 0:
-            make_rel(with_node=p,code_field_name='PlaceOfInfection_EN',lookup_dict=countries, relation_name="PlaceOfInfection",
-                     rel_node_label="Country")
+def load_data(datafile, logwriter):
+    host = input("Please enter the neo4j database server IP (defaults to localhost): ")
+    host = 'localhost' if len(host.strip(' ')) == 0 else host
+    password = input("Please enter the neo4j user's database password: ")
+    graph = Graph("bolt://{}:7687".format(host), user='neo4j',
+                  password=password)  # this may need to change when running from the server since there it needs the instance IP
+    graph.delete_all()
+    dims = create_dims(graph)
 
-# p = Node("Person", ssi_id="example_id")
-# tx.create(Relationship(p,"ISA",sex_m))
-# tx.create(Relationship(p,"ISA",age_groups['40-49']))
-# p2 = Node("Person", ssi_id="example_id2")
-# tx.create(Relationship(p2,"ISA",sex_m))
-# tx.create(Relationship(p2,"ISA",age_groups['40-49']))
-tx.commit()
+    # Create data
+    tx = graph.begin()
+    with open(datafile) as file:
+        reader = csv.DictReader(file, delimiter='\t')
+        i = 0
+        for row in reader:
+            i += 1
+            age = row['ReportAge'] if row['ReportAge'] == '' else int(row['ReportAge'])
+            cv_stat = row['COVID19_Status'] if len(row['COVID19_Status']) > 0 else '0'  # error correction
+            p = Node("Person", ssi_id=row['ssi_id'], age=age, COVID19_Status=cv_stat,
+                     COVID19_EndDate=row['COVID19_EndDate'], IsPregnant=(row['Pregnancy'] == '1')
+                     , sequenced=(row['sequenced'] == 'Yes'), SymptomsStartDate=row['SymptomsStartDate']
+                     , SampleDate=row['SampleDate'])
 
-print("Successfully created graph from metadata file.")
+            if row['Pregnancy'] == '1' and row['Sex'] == 'M':
+                log_field_error('anomalous case data', i,
+                                'SSI {}, Pregnancy {}, Sex {}'.format(row['ssi_id'], row['Pregnancy'], row['Sex']),
+                                logwriter)  # TODO extract all error checking code to a separate file
+
+            tx.create(p)
+            if row['Sex'] == 'F':
+                tx.create(Relationship(p, "ISA", dims['sex_f']))
+            elif row['Sex'] == 'M':
+                tx.create(Relationship(p, "ISA", dims['sex_m']))
+            # else: # make error log file
+            #  print('Unrecognized Sex value: {}'.format(row['Sex']))
+            ag = row['ReportAgeGrp']
+            if ag in dims['age_groups'].keys():
+                tx.create(Relationship(p, "InGroup", dims['age_groups'][ag]))
+            # TODO report missing ag
+
+            # Parish
+            parish = make_rel(tx, row, with_node=p, code_field_name='Parishcode', lookup_dict=dims['parishes'],
+                              relation_name="LivesIn",
+                              rel_node_label="Parish", name_field_name="ParishName")
+
+            # Municipality
+            if parish is not None:
+                muni = make_rel(tx, row, with_node=parish, code_field_name='MunicipalityCode',
+                                lookup_dict=dims['municipalities'], relation_name="PartOf",
+                                rel_node_label="Municipality")
+
+            # NUTS3 Region
+            if muni is not None:
+                make_rel(tx, row, with_node=muni, code_field_name='NUTS3Code', lookup_dict=dims['nuts3_regions'],
+                         relation_name="PartOf",
+                         rel_node_label="NUTS3_Region", name_field_name="NUTS3Text")
+
+            # strains
+            strain_name = row['lineage']
+            if len(strain_name) > 0:
+                make_rel(tx, row, with_node=p, code_field_name='lineage', lookup_dict=dims['strains'],
+                         relation_name="HasStrain",
+                         rel_node_label="Strain")
+
+            # Risk factors
+            for field_name in dims['risk_factors'].keys():
+                if row[field_name] in ['SAND', 'TRUE']:
+                    tx.create(Relationship(p, "HasRisk", dims['risk_factors'][field_name]))
+
+            # Place of infection
+            country = row['PlaceOfInfection_EN']
+            if len(country) > 0:
+                make_rel(tx, row, with_node=p, code_field_name='PlaceOfInfection_EN', lookup_dict=dims['countries'],
+                         relation_name="PlaceOfInfection",
+                         rel_node_label="Country")
+
+    tx.commit()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='load a metadata file to neo4j')
+    parser.add_argument('infile', type=str, help='path to the file to be checked')
+    parser.add_argument('errfile', type=str, help='path to the error file to be created')
+    args = parser.parse_args()
+    infile = check_file(args.infile)
+    print('Validated infile as {}'.format(infile))
+    errfile = check_file(args.errfile, True)
+    print('Validated errfile as {}'.format(errfile))
+    with open(errfile, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, ['MessageType', 'Row', 'ErrorType', 'Details'])
+        writer.writeheader()
+        writer.writerow({'MessageType': 'Info', 'ErrorType': '', 'Details': 'Started {}'.format(infile)})
+        load_data(infile, writer)
+        writer.writerow({'MessageType': 'Info', 'ErrorType': '', 'Details': 'Finished {}'.format(infile)})
