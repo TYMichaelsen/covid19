9957595846e0e194dd0dde5ae10deae918b6911f
Tomer Sagi <ts.tomersagi@gmail.com>
2020-09-03 14:00:30 +0200

    added clades to mysql 6

--- bi_system/to_microreact.py
+++ bi_system/to_microreact.py
@@ -1,5 +1,5 @@
-sql = 'SELECT week(date), C.low_res, R.name, count(*) as cases, R.longitude, R.latitude ' \
+sql = 'SELECT week(date), C.low_res_clade, R.name, count(*) as cases, R.longitude, R.latitude ' \
       'FROM Persons P JOIN Municipalities M ON P.MunicipalityCode=M.code JOIN NUTS3_Regions R ON M.region=R.code ' \
       'JOIN Clade_assignment C ON P.ssi_id =  C.ssi_id' \
-      'GROUP BY week(date), lineage, R.name' \
+      'GROUP BY week(date), C.low_res_clade, R.name' \
       ';' # TBD
