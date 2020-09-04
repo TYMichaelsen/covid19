5cfcc6508f28b265b4900d75ef467aef4134f7e9
Tomer Sagi <ts.tomersagi@gmail.com>
2020-09-04 12:52:40 +0200

    to_microreact query now on a sample level

--- bi_system/to_microreact.py
+++ bi_system/to_microreact.py
@@ -28,11 +28,10 @@ def select(con, query):
 
 con = get_connection(con_dict)
 
-query = 'SELECT week(date), C.low_res_clade, R.name, count(*) as cases, R.longitude, R.latitude, day(date), month(date), year(date)' \
+query = 'SELECT P.ssi_id, week(date), C.low_res_clade, R.name, R.longitude, R.latitude, day(date), month(date), year(date)' \
       'FROM Persons P JOIN Municipalities M ON P.MunicipalityCode=M.code JOIN NUTS3_Regions R ON M.region=R.code ' \
       'JOIN Clade_assignment C ON P.ssi_id =  C.ssi_id ' \
-      'GROUP BY week(date), C.low_res_clade, R.name' \
-      ';' # TBD
+      ';'
 
 result = select(con, query)
 
