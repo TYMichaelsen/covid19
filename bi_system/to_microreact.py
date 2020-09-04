b9add26cf4581f82ad9ea1593672ce7948cc904e
Tomer Sagi <tsagi@cs.aau.dk>
2020-09-04 12:40:31 +0200

    added config elements

--- bi_system/to_microreact.py
+++ bi_system/to_microreact.py
@@ -1,7 +1,7 @@
 import csv
 from random import randint, choice
 
-from .convert_metadata_to_mysql import get_connection
+from convert_metadata_to_mysql import get_connection
 from mysql.connector import Error
 
 
@@ -30,7 +30,7 @@ con = get_connection(con_dict)
 
 query = 'SELECT week(date), C.low_res_clade, R.name, count(*) as cases, R.longitude, R.latitude, day(date), month(date), year(date)' \
       'FROM Persons P JOIN Municipalities M ON P.MunicipalityCode=M.code JOIN NUTS3_Regions R ON M.region=R.code ' \
-      'JOIN Clade_assignment C ON P.ssi_id =  C.ssi_id' \
+      'JOIN Clade_assignment C ON P.ssi_id =  C.ssi_id ' \
       'GROUP BY week(date), C.low_res_clade, R.name' \
       ';' # TBD
 
