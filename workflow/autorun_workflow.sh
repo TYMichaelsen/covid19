cae344ba9b1bda061305ffa1a4774a1cab55b816
TYMichaelsen <tym@bio.aau.dk>
2020-09-22 12:22:14 +0200

    changed timing

--- workflow/autorun_workflow.sh
+++ workflow/autorun_workflow.sh
@@ -16,29 +16,29 @@ while [ -s /srv/rbd/covid19/processing/missing.txt ]; do
   
   DIR=$(head -n 1 /srv/rbd/covid19/processing/missing.txt)
   
-  echo "[$(date +"%T")] Found a new batch: $DIR"
+  echo "[$(date +"%T")] Found a new batch: $(basename $DIR)"
   
   # Check if the folder is static (i.e. no more output recived from AI node).
   
   while : ; do
     old_files=$(find $DIR)
     
-    echo "[$(date +"%T")] Wait to check if $DIR is static. Check again in 5 minutes."
+    echo "[$(date +"%T")] Wait to check if $(basename $DIR) is static. Check again in 5 minutes."
     
-    sleep 5m 
+    sleep 2
     
     new_files=$(find $DIR)
     
     diff_files=$(diff -q <(echo $old_files) <(echo $new_files))
     
     if [ -z "$diff_files" ]; then 
-      echo "[$(date +"%T")] $DIR is static, continue with workflow processing." 
+      echo "[$(date +"%T")] $(basename $DIR) is static, continue with workflow processing." 
       break
     fi
   done
   
   # Run the workflow.  
-  covid19_workflow.sh -i $DIR -s aau_long_v3.1
+  covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1
   
   # Remove from missing.txt and add to processed.txt
   tail -n +2 /srv/rbd/covid19/processing/missing.txt > /srv/rbd/covid19/processing/missing.txt
