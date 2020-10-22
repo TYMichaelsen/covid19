741c7a30311da67c70faaf64c20fcbd72b568b2a
TYMichaelsen <tym@bio.aau.dk>
2020-10-22 10:15:56 +0200

    Added control of data size and completeness

--- workflow/autorun_workflow.sh
+++ workflow/autorun_workflow.sh
@@ -1,11 +1,13 @@
 #!/bin/bash 
 
-# This script checks for new batches in /srv/rbd/covid19/processing. If a new batch is present, it monitors for a while - if the folder is static it starts the covid19_workflow.sh script.
-#
-# 
+# This script checks for new batches in /srv/rbd/covid19/processing. If a new batch is present, it monitors for a while - if the folder is static and has >20Gb it starts the covid19_workflow.sh script.
 
-# FIRST USE ONLY: Make a date-sorted text file with all the batches processed so far.
-# ls -dtr /srv/rbd/covid19/processing/?J* > /srv/rbd/covid19/processing/processed.txt
+# List all batches processed so far. Criteria for proccesed is consensus.fasta in /final_output/ folder.
+for batch in $(ls -dtr /srv/rbd/covid19/processing/?J*); do 
+  if [ -f $batch/final_output/consensus.fasta ]; then
+    echo $batch
+  fi
+done > /srv/rbd/covid19/processing/processed.txt
 
 while : ; do
 
@@ -16,14 +18,14 @@ while [ -s /srv/rbd/covid19/processing/missing.txt ]; do
   
   DIR=$(head -n 1 /srv/rbd/covid19/processing/missing.txt)
   
-  echo "[$(date +"%T")] Found a new batch: $(basename $DIR)"
+  echo "[$(date +"%b %d %T")] Found a new batch: $(basename $DIR)"
   
-  # Check if the folder is static (i.e. no more output recived from AI node).
+  # Check if the folder is static (i.e. no more output recived from elsewhere).
   
   while : ; do
     old_files=$(find $DIR)
     
-    echo "[$(date +"%T")] Wait to check if $(basename $DIR) is static. Check again in 5 minutes."
+    echo "[$(date +"%b %d %T")] Wait to check if $(basename $DIR) is static. Check again in 5 minutes."
     
     sleep 5m
     
@@ -32,21 +34,36 @@ while [ -s /srv/rbd/covid19/processing/missing.txt ]; do
     diff_files=$(diff -q <(echo $old_files) <(echo $new_files))
     
     if [ -z "$diff_files" ]; then 
-      echo "[$(date +"%T")] $(basename $DIR) is static, continue with workflow processing." 
+      echo "[$(date +"%b %d %T")] $(basename $DIR) is static, continue with workflow processing." 
       break
     fi
-  done
-  
-  # Run the workflow.  
-  covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1
-  
-  # Remove from missing.txt and add to processed.txt
-  tail -n +2 /srv/rbd/covid19/processing/missing.txt > tmp && mv tmp /srv/rbd/covid19/processing/missing.txt
-  cat /srv/rbd/covid19/processing/processed.txt <(echo $DIR) > tmp && mv tmp /srv/rbd/covid19/processing/processed.txt
-   
+  done  
+
+  # Test if there is >20 Gb data. If not, cancel the job. 
+  nfastq=$(find $DIR/fastq -name *fastq | wc -l)
+  nbases=$(( $nfastq * 4000 * 1050 ))
+
+  if [ $nbases -ge 20000000000 ]; then
+
+    # Run the workflow.  
+    covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1
+
+    # Remove from top of missing.txt and add to processed.txt
+    tail -n +2 /srv/rbd/covid19/processing/missing.txt > tmp && mv tmp /srv/rbd/covid19/processing/missing.txt
+    cat /srv/rbd/covid19/processing/processed.txt <(echo $DIR) > tmp && mv tmp /srv/rbd/covid19/processing/processed.txt
+
+  else
+    
+    echo "[$(date +"%b %d %T")] $(basename $DIR) is static but has <20 Gb data, adding it to bottom of missing.txt and continue." 
+    
+    # Remove from top of missing.txt and add to bottom of missing.txt
+    tail -n +2 /srv/rbd/covid19/processing/missing.txt       > tmp && mv tmp /srv/rbd/covid19/processing/missing.txt
+    cat /srv/rbd/covid19/processing/missing.txt <(echo $DIR) > tmp && mv tmp /srv/rbd/covid19/processing/missing.txt
+  fi
+
 done
 
-echo "[$(date +"%T")] No new batches found. Check again in 30 minutes." 
+echo "[$(date +"%b %d %T")] No new batches found. Check again in 30 minutes." 
 
 sleep 30m
 
