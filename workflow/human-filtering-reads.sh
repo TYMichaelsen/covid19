55c99869d5453499bee79b34124b1140c57ea464
TYMichaelsen <tym@bio.aau.dk>
2020-11-16 21:02:38 +0100

    script that removes human reads from fastq files

--- workflow/human-filtering-reads.sh
+++ workflow/human-filtering-reads.sh
@@ -2,12 +2,12 @@
 
 source activate artic-ncov2019
 
-touch $INDIR/human-filtered.txt
-
 # for each fastq, run sanitizeme.
 parallel -j $THREADS '
 
-minimap2 -ax map-ont -t 1 {2} {1} | samtools view --threads 1 -u -f 4 - | samtools fastq --threads - > tmp && mv tmp {1}
-echo {1} >> $INDIR/human-filtered.txt
+minimap2 -ax map-ont -t 1 {2} {1} | samtools view --threads 1 -u -f 4 - | samtools fastq --threads - > {1}.tmp && mv {1}.tmp {1}
 
 ' ::: $INDIR/*fastq ::: $HUMREF
+
+# Save that the files has been filtered.
+cat $INDIR/*fastq.txt > $INDIR/human-filtered.txt
