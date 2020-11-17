1bd91a27d0847d1ca495aa3f3a69e06c1a84ed69
TYMichaelsen <tym@bio.aau.dk>
2020-11-17 13:39:15 +0100

    Snippet to perform human filtering

---
+++ workflow/human-filtering-reads.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source activate artic-ncov2019-medaka
+
+# for each fastq, run sanitizeme.
+parallel -j $THREADS '
+
+minimap2 -ax map-ont -t 1 {2} {1} | samtools view --threads 1 -u -f 4 - | samtools fastq --threads - > {1}.tmp && mv {1}.tmp {1}
+
+' ::: $INDIR/*fastq ::: $HUMREF
+
+# Save that the files has been filtered.
+echo "Data has been human filtered" > $INDIR/filtered.txt
