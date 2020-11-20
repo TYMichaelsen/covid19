9bfd2393164f11ac8161d1d303f7b6e8e9e5a1fd
TYMichaelsen <tym@bio.aau.dk>
2020-11-20 09:12:34 +0100

    added logging

--- workflow/human-filtering-reads.sh
+++ workflow/human-filtering-reads.sh
@@ -2,10 +2,17 @@
 
 source activate artic-ncov2019-medaka
 
+# Logging
+LOG_NAME="$INDIR/filtering_log_$(date +"%Y-%m-%d_%H-%M").txt"
+echo "filtering log" >> $LOG_NAME
+echo "Command: $0 $*" >> $LOG_NAME
+exec &> >(tee -a "$LOG_NAME")
+exec 2>&1
+
 # for each fastq, run sanitizeme.
 parallel -j $THREADS '
 
-minimap2 -ax map-ont -t 1 {2} {1} | samtools view --threads 1 -u -f 4 - | samtools fastq --threads - > {1}.tmp && mv {1}.tmp {1}
+minimap2 -ax map-ont -t 1 {2} {1} | samtools view --threads 1 -u -f 4 - | samtools fastq --threads 1 - > {1}.tmp && mv {1}.tmp {1}
 
 ' ::: $INDIR/*fastq ::: $HUMREF
 
