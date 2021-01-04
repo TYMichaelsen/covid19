89014e434f53e4fa0cfe8da5be6db81775a79615
TYMichaelsen <tym@bio.aau.dk>
2021-01-04 13:40:00 +0100

    better log-messaging and allow manual override

--- workflow/autorun_workflow.sh
+++ workflow/autorun_workflow.sh
@@ -9,7 +9,14 @@ cd /srv/rbd/covid19/processing
 # - /srv/data_1/gridion
 
 # If a new batch is present, it monitors for a while -
-# if the folder is static and has enough data it starts the covid19_workflow.sh script.
+# if the folder has enough data it starts the covid19_workflow.sh script.
+
+# How many minutes between echo'ing that the process is still going.
+LIFESIGN=30 
+ATTEMPT=0
+
+rm missing.txt
+rm processed.txt
 
 # Logging
 LOG_NAME="/srv/rbd/covid19/processing/autorun_log_$(date +"%Y-%m-%d_%H-%M").txt"
@@ -20,8 +27,12 @@ exec 2>&1
 
 chmod 777 $LOG_NAME
 
+echo "[$(date +"%b %d %T")] Started waiting for new batches. Will check every 2 min." 
+
+while : ; do
+
 # List all batches processed so far. Criteria for proccesed is a non-empty consensus.fasta in /final_output/ folder.
-for batch in $(ls -dtr /srv/rbd/covid19/processing/?J*) $(ls -dtr /srv/data_1/?J*) $(ls -dtr /srv/data_1/gridion/?J*); do
+for batch in $(ls -dtr /srv/rbd/covid19/processing/?J*/) $(ls -dtr /srv/data_1/?J*/) $(ls -dtr /srv/data_1/gridion/?J*/); do
   if [ -s $batch/final_output/consensus.fasta ]; then
     echo $batch
   fi
@@ -29,70 +40,70 @@ done > /srv/rbd/covid19/processing/processed.txt
 
 chmod 777 /srv/rbd/covid19/processing/processed.txt
 
-while : ; do
-
-# Check if a new batch has arrived.
-comm -23 <(ls -d /srv/rbd/covid19/processing/?J* /srv/data_1/?J* /srv/data_1/gridion/?J* | sort) <(sort /srv/rbd/covid19/processing/processed.txt) > /srv/rbd/covid19/processing/missing.txt
+# List all batches NOT processed.
+comm -23 <(ls -d /srv/rbd/covid19/processing/?J*/ /srv/data_1/?J*/ /srv/data_1/gridion/?J*/ | sort) <(sort /srv/rbd/covid19/processing/processed.txt) > /srv/rbd/covid19/processing/missing.txt
 
 chmod 777 /srv/rbd/covid19/processing/missing.txt
 
-while [ -s /srv/rbd/covid19/processing/missing.txt ]; do 
+if [ -s /srv/rbd/covid19/processing/missing.txt ]; then 
+
+  ATTEMPT=0
+
+  while read DIR; do 
   
-  DIR=$(head -n 1 /srv/rbd/covid19/processing/missing.txt)
-  
-  echo "[$(date +"%b %d %T")] Found a new batch: $(basename $DIR)"
-  
-  ### Check if the folder is static (i.e. no more output recived from elsewhere).------------------------------------
-  old_files=$(find $DIR)
+    echo "[$(date +"%b %d %T")] Found a new batch: $(basename $DIR)"
     
-  echo "[$(date +"%b %d %T")] Wait 2 min to check if $(basename $DIR) is static."
+    ### Check if the folder is static (i.e. no more output recived from elsewhere).------------------------------------
+    old_files=$(find $DIR)
     
-  sleep 2m
+    echo "[$(date +"%b %d %T")] Wait 2 min to check if $(basename $DIR) is static."
+
+    sleep 2m
     
-  new_files=$(find $DIR)
+    new_files=$(find $DIR)
    
-  diff_files=$(diff -q <(echo $old_files) <(echo $new_files))
+    diff_files=$(diff -q <(echo $old_files) <(echo $new_files))
     
-  if [ -z "$diff_files" ]; then 
-    echo "[$(date +"%b %d %T")] $(basename $DIR) is static, continue with workflow processing." 
+    if [ -z "$diff_files" ]; then 
 
-    ### Test if there is enough data. If not, cancel the job.----------------------------------------------------------- 
-    nfastq=$(find $DIR/fastq -name *fastq | wc -l)
-    nbases=$(( $nfastq * 4000 * 1050 ))
+      ### Test if there is enough data. If not, cancel the job.----------------------------------------------------------- 
+      nfastq=$(find $DIR/fastq -name *fastq | wc -l)
+      nbases=$(( $nfastq * 4000 * 1050 ))
     
-    # Grep the number Gb specified by user. If none, the default 20Gb are used.
-    if [ -s $DIR/datacap.txt ]; then DATACAP=$(($(cat $DIR/datacap.txt) * 1000000000)); else DATACAP=20000000000; fi
+      # Grep the number Gb specified by user. If none, the default 20Gb are used.
+      if [ -s $DIR/datacap.txt ]; then DATACAP=$(($(cat $DIR/datacap.txt) * 1000000000)); else DATACAP=20000000000; fi
     
-    if [ $nbases -ge $DATACAP ]; then
-
-      # Run the workflow. 
-      cd $(dirname $DIR) 
-      covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1
-
-      # Remove from top of missing.txt and add to processed.txt
-      tail -n +2 /srv/rbd/covid19/processing/missing.txt > tmp_auto && mv tmp_auto /srv/rbd/covid19/processing/missing.txt
-      cat /srv/rbd/covid19/processing/processed.txt <(echo $DIR) > tmp_auto && mv tmp_auto /srv/rbd/covid19/processing/processed.txt
-
-    else
-    
-      echo "[$(date +"%b %d %T")] $(basename $DIR) is static but has <20 Gb data, adding it to bottom of missing.txt and continue." 
-    
-      # Remove from top of missing.txt and add to bottom of missing.txt
-      tail -n +2 /srv/rbd/covid19/processing/missing.txt       > tmp_auto && mv tmp_auto /srv/rbd/covid19/processing/missing.txt
-      cat /srv/rbd/covid19/processing/missing.txt <(echo $DIR) > tmp_auto && mv tmp_auto /srv/rbd/covid19/processing/missing.txt
+      if [ $nbases -ge $DATACAP ]; then
+        
+        if [ -z "$(pgrep -fl covid19_workflow.sh)" ]; then
+          
+          # Run the workflow. 
+          echo "[$(date +"%b %d %T")] $(basename $DIR) is static, continue with workflow processing." 
+          cd $(dirname $DIR) 
+          covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1 >/dev/null 2>&1
+          echo "[$(date +"%b %d %T")] workflow completed for $(basename $DIR)." 
+        else 
+          echo "[$(date +"%b %d %T")] covid19_workflow.sh is running elsewhere. Skipping and continue." 
+        fi  
+      else
+        echo "[$(date +"%b %d %T")] $(basename $DIR) is static but has <20 Gb data, skipping and continue." 
+      fi
+    else 
+      echo "[$(date +"%b %d %T")] $(basename $DIR) was not static, skipping and continue." 
     fi
-  else 
-    echo "[$(date +"%b %d %T")] $(basename $DIR) was not static, adding it to bottom of missing.txt and continue." 
+  
+  done < /srv/rbd/covid19/processing/missing.txt
 
-    # Remove from top of missing.txt and add to bottom of missing.txt
-    tail -n +2 /srv/rbd/covid19/processing/missing.txt       > tmp_auto && mv tmp_auto /srv/rbd/covid19/processing/missing.txt
-    cat /srv/rbd/covid19/processing/missing.txt <(echo $DIR) > tmp_auto && mv tmp_auto /srv/rbd/covid19/processing/missing.txt
+  echo "[$(date +"%b %d %T")] Started waiting for new batches. Will check every 2 min." 
+
+else 
+  ATTEMPT=$(($ATTEMPT+1))
+  
+  if [ ! $ATTEMPT -lt $LIFESIGN ]; then
+    echo "[$(date +"%b %d %T")] Still waiting!"
+    ATTEMPT=0
   fi
-
-done
-
-echo "[$(date +"%b %d %T")] No new batches found. Check again in 30 minutes." 
-
-sleep 30m
+  sleep 1m
+fi
 
 done
