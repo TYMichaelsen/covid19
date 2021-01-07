94ee481871451eb0523ac3c66e844d96874d15a4
TYMichaelsen <tym@bio.aau.dk>
2021-01-07 17:09:24 +0100

    removed process checking and fixed correct sorting for forced runs

--- workflow/autorun_workflow.sh
+++ workflow/autorun_workflow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 FORCE_START=${1:-2500}
-LAST_TIME=$(($(date +%H%M) - 2 ))
+LAST_TIME=$(($(date +%H%M | sed 's/^0*//') - 2 ))
 
 # Need to work here to get permission to make temporary files.
 cd /srv/rbd/covid19/processing
@@ -14,11 +14,12 @@ cd /srv/rbd/covid19/processing
 # if the folder has enough data it starts the covid19_workflow.sh script.
 
 # How many minutes between echo'ing that the process is still going.
-LIFESIGN=1
+LIFESIGN=60
 ATTEMPT=0
 
-rm missing.txt
-rm processed.txt
+rm /srv/rbd/covid19/processing/missing.txt
+rm /srv/rbd/covid19/processing/processed.txt
+rm /srv/rbd/covid19/processing/forcestart.txt 
 
 # Logging
 LOG_NAME="/srv/rbd/covid19/processing/autorun_log_$(date +"%Y-%m-%d_%H-%M").txt"
@@ -33,7 +34,7 @@ echo "[$(date +"%b %d %T")] Started waiting for new batches. Will check every 2 
 
 while : ; do
   
-  CUR_TIME=$(date +%H%M)
+  CUR_TIME=$(date +%H%M | sed 's/^0*//')
 
   # List all batches processed so far. Criteria for proccesed is a non-empty consensus.fasta in /final_output/ folder.
   for batch in $(ls -dtr /srv/rbd/covid19/processing/?J*/) $(ls -dtr /srv/data_1/?J*/) $(ls -dtr /srv/data_1/gridion/?J*/); do
@@ -50,6 +51,9 @@ while : ; do
   chmod 777 /srv/rbd/covid19/processing/missing.txt
 
   if [ -s /srv/rbd/covid19/processing/missing.txt ]; then 
+    
+    echo "[$(date +"%b %d %T")] found new batches $(while read DIR; do echo $(basename $DIR); done < /srv/rbd/covid19/processing/missing.txt | paste -sd, -)."
+    echo "[$(date +"%b %d %T")] checking for enough data."
 
     # Before doing anything, check that timepoint for forced start has not passed.
     if [ $FORCE_START -gt $LAST_TIME ] && [ $FORCE_START -le $CUR_TIME ]; then
@@ -62,29 +66,27 @@ while : ; do
 
         echo -e $DIR"\t"$nbases
       done < /srv/rbd/covid19/processing/missing.txt |
-      sort -r -k2,2 | 
-      cut -f1 > tmp && mv tmp /srv/rbd/covid19/processing/missing.txt
+      sort -rn -k2,2 | 
+      cut -f1 > /srv/rbd/covid19/processing/forcestart.txt 
+      
+      chmod 777 /srv/rbd/covid19/processing/forcestart.txt 
 
       # Run workflow for all.
       while read DIR; do 
-        if [ -z "$(pgrep -fl covid19_workflow.sh)" ]; then
-          cd $(dirname $DIR) 
-          #--------------------------------------------------------------------------------
-          covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1 
-          #mkdir $DIR/final_output
-          #echo "hest" > $DIR/final_output/consensus.fasta
-          #--------------------------------------------------------------------------------
-          echo "[$(date +"%b %d %T")] workflow completed for $(basename $DIR)." 
-        fi
-      done < /srv/rbd/covid19/processing/missing.txt
+        cd $(dirname $DIR) 
+        #--------------------------------------------------------------------------------
+        covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1 
+        #mkdir $DIR/final_output
+        #echo "hest" > $DIR/final_output/consensus.fasta
+        #--------------------------------------------------------------------------------
+        echo "[$(date +"%b %d %T")] workflow completed for $(basename $DIR)." 
+      done < /srv/rbd/covid19/processing/forcestart.txt
     else
 
       ATTEMPT=0
 
       while read DIR; do 
-    
-        echo "[$(date +"%b %d %T")] Found a new batch: $(basename $DIR)"
-  
+     
         ### Test if there is enough data. If not, cancel the job.----------------------------------------------------------- 
         nfastq=$(find $DIR/fastq -name *.fastq | wc -l)
         nbases=$(( $nfastq * 4000 * 1050 ))
@@ -93,28 +95,20 @@ while : ; do
         if [ -s $DIR/datacap.txt ]; then DATACAP=$(($(cat $DIR/datacap.txt) * 1000000000)); else DATACAP=20000000000; fi
     
         if [ $nbases -ge $DATACAP ]; then
-         
-          if [ -z "$(pgrep -fl covid19_workflow.sh)" ]; then
-          
-            # Run the workflow. 
-            echo "[$(date +"%b %d %T")] $(basename $DIR) has enough data, continue with workflow processing." 
-            cd $(dirname $DIR) 
-            #--------------------------------------------------------------------------------
-            covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1
-            #mkdir $DIR/final_output
-            #echo "hest" > $DIR/final_output/consensus.fasta
-            #--------------------------------------------------------------------------------
-            echo "[$(date +"%b %d %T")] workflow completed for $(basename $DIR)." 
-          else 
-            echo "[$(date +"%b %d %T")] covid19_workflow.sh is running elsewhere. Skipping and continue." 
-          fi  
+                  
+          # Run the workflow. 
+          echo "[$(date +"%b %d %T")] $(basename $DIR) has enough data, continue with workflow processing." 
+          cd $(dirname $DIR) 
+          #--------------------------------------------------------------------------------
+          covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1
+          #mkdir $DIR/final_output
+          #echo "hest" > $DIR/final_output/consensus.fasta
+          #--------------------------------------------------------------------------------
+          echo "[$(date +"%b %d %T")] workflow completed for $(basename $DIR)." 
         fi
-        sleep 3
       done < /srv/rbd/covid19/processing/missing.txt
     fi
-
-    echo "[$(date +"%b %d %T")] Started waiting for new batches. Will check every 2 min." 
-
+    sleep 2m
   else 
     ATTEMPT=$(($ATTEMPT+1))
   
