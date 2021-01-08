4ad5b1ba621dc7cafedf8a4362c5d8bc2f5840be
TYMichaelsen <tym@bio.aau.dk>
2021-01-08 14:26:25 +0100

    enable run workflow

--- workflow/redo_workflow.sh
+++ workflow/redo_workflow.sh
@@ -14,8 +14,7 @@ while read DIR; do
   
   if [ ! -z "$RERUN" ]; then
     cd $(dirname $DIR) 
-    #covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1
-    echo "Running!"
+    covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1
   else
     if [ -s $DIR/data_used.txt ] && [ -s $DIR/datacap.txt ]; then
       CAP=$(($(cat $DIR/datacap.txt) * 1000000000))
@@ -24,8 +23,7 @@ while read DIR; do
         echo "[$(basename $DIR)] This batch has been run with enough data, skipping."
       else 
         cd $(dirname $DIR) 
-	#covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1
-        echo "Running!"
+	covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1
       fi
     else
       echo "[$(basename $DIR)] Either data_used.txt and/or datacap.txt was not found, don't know what to do. You will have to manually handle this."
