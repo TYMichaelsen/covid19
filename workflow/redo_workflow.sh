68cf751ddc66f111e43e16b26b4e15883f1b24f0
TYMichaelsen <tym@bio.aau.dk>
2021-01-10 13:54:18 +0100

    added ordering according to data size when 'rerun' option enabled

--- workflow/redo_workflow.sh
+++ workflow/redo_workflow.sh
@@ -7,10 +7,20 @@ RERUN=$2
 # - The workflow was started before full data was reached
 # - The workflow needs to be rerun
 
-# Takes as input the string 'rerun' to force-run workflow completely.
+# Reorder batches according to amount of data.
+BATCHES_ORD=$(while read DIR; do
+  nfastq=$(find $DIR/fastq -name *fastq | wc -l)
+  nbases=$(( $nfastq * 4000 * 1050 ))
 
-while read DIR; do
-  
+  echo -e $DIR"\t"$nbases
+
+done < $BATCHES |
+sort -rn -k2,2 | 
+cut -f1 -)
+
+# Takes as input the string 'rerun' to force-run workflow completely.
+for DIR in $BATCHES_ORD; do 
+ 
   if [ ! -z "$RERUN" ]; then
     echo "[$(basename $DIR)] force runnning from scratch!"
      
@@ -20,7 +30,9 @@ while read DIR; do
     rm -rf $DIR/final_output
 
     cd $(dirname $DIR) 
+    echo "[$(basename $DIR)] Running workflow!"
     covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1
+
   else
     if [ -s $DIR/data_used.txt ] && [ -s $DIR/datacap.txt ]; then
       CAP=$(($(cat $DIR/datacap.txt) * 1000000000))
@@ -29,10 +41,11 @@ while read DIR; do
         echo "[$(basename $DIR)] This batch has been run with enough data, skipping."
       else 
         cd $(dirname $DIR) 
+        echo "[$(basename $DIR)] Running workflow!"
 	covid19_workflow.sh -i $(basename $DIR) -s aau_long_v3.1
       fi
     else
       echo "[$(basename $DIR)] Either data_used.txt and/or datacap.txt was not found, don't know what to do. You will have to manually handle this."
     fi
   fi
-done < $BATCHES
+done 
